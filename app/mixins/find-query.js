e9ac8cc3d1077497d415882a8592bdab384a9233
Tom <tgh8xn@virginia.edu>
2017-01-07 18:43:16 -0500

    app/find-query imports addon/find-query

---
+++ app/mixins/find-query.js
@@ -0,0 +1,8 @@
+/*
+  This file exists so users don't need to manually import the mixin after installing the addon
+*/
+
+import Ember from 'ember';
+import FindQueryMixin from 'ember-emberfire-find-query/mixins/find-query';
+
+export default FindQueryMixin;
