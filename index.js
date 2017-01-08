4f493400d881a9daf12496150be13469ad55411c
Tom <tgh8xn@virginia.edu>
2017-01-07 21:01:37 -0500

    Commenting unnecessary function in index.js

--- index.js
+++ index.js
@@ -3,9 +3,10 @@
 
 module.exports = {
   name: 'ember-emberfire-find-query',
-
+/*
   included: function(app) {
     this._super.included(app);
     app.import('addon/mixins/find-query.js');
   }
+*/
 };
